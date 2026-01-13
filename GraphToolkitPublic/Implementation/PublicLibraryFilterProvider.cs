a4640f5b12e5574eaaf5a0b9f182844244d3b8ec
anttirt <anttirt@anttirt.net>
2026-01-13 23:40:05 +0200

    filter nodes in context menu to nodes that have a compatible port

---
+++ GraphToolkitPublic/Implementation/PublicLibraryFilterProvider.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unity.GraphToolkit.ItemLibrary.Editor;
+
+namespace Unity.GraphToolkit.Editor.Implementation
+{
+    internal class PublicLibraryFilterProvider : ILibraryFilterProvider
+    {
+        public ItemLibraryFilter GetContextFilter(ContextNodeModel contextNodeModel) => ItemLibraryFuncFilter.Empty;
+        public ItemLibraryFilter GetGraphFilter() => ItemLibraryFuncFilter.Empty;
+
+        public ItemLibraryFilter GetInputToGraphFilter(IEnumerable<PortModel> portModels)
+            => new ItemLibraryFuncFilter(item => MatchPorts(portModels, item));
+
+        public ItemLibraryFilter GetInputToGraphFilter(PortModel portModel)
+            => new ItemLibraryFuncFilter(item => MatchPorts(new[] { portModel }, item));
+
+        public ItemLibraryFilter GetOutputToGraphFilter(IEnumerable<PortModel> portModels)
+            => new ItemLibraryFuncFilter(item => MatchPorts(portModels, item));
+
+        public ItemLibraryFilter GetOutputToGraphFilter(PortModel portModel)
+            => new ItemLibraryFuncFilter(item => MatchPorts(new[] { portModel }, item));
+
+        public ItemLibraryFilter GetWireFilter(WireModel wireModel) => ItemLibraryFuncFilter.Empty;
+
+        private static bool MatchPorts(IEnumerable<PortModel> portModels, ItemLibraryItem item)
+        {
+            if(item is not GraphNodeModelLibraryItem nodeItem)
+                return true;
+
+            var portModel = portModels.FirstOrDefault();
+            if(portModel == null)
+                return true;
+
+            var graphModel = portModel.GraphModel;
+
+            var nodeModel = CreateGraphElementModel(graphModel, nodeItem) as NodeModel;
+            if(nodeModel == null)
+                return true;
+
+            var ports = portModel.Direction == PortDirection.Input ? nodeModel.OutputsByDisplayOrder : nodeModel.InputsByDisplayOrder;
+            var compatiblePorts = graphModel.GetCompatiblePorts(ports, portModel);
+
+            return compatiblePorts.Count > 0;
+        }
+
+        protected static GraphElementModel CreateGraphElementModel(GraphModel graphModel, GraphNodeModelLibraryItem item)
+        {
+            return item.CreateElement(
+                new GraphNodeCreationData(graphModel, UnityEngine.Vector2.zero, SpawnFlags.Orphan));
+        }
+
+    }
+}
--- GraphToolkitPublic/Implementation/PublicLibraryHelper.cs
+++ GraphToolkitPublic/Implementation/PublicLibraryHelper.cs
@@ -2,10 +2,17 @@ namespace Unity.GraphToolkit.Editor.Implementation
 {
     class PublicLibraryHelper : ItemLibraryHelper
     {
+        private ILibraryFilterProvider m_LibraryFilterProvider;
+
         public PublicLibraryHelper(GraphModel graphModel) : base(graphModel) { }
         public override IItemDatabaseProvider GetItemDatabaseProvider()
         {
             return m_DatabaseProvider ??= new PublicDatabaseProviderImp(GraphModel);
+        }
+
+        public override ILibraryFilterProvider GetLibraryFilterProvider()
+        {
+            return m_LibraryFilterProvider ??= new PublicLibraryFilterProvider();
         }
     }
 }
